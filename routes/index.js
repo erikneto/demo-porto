44296efd32b1ceb2289a0adb84a48d3647f71c09
Erik Andersen Neto <erikneto@br.ibm.com>
2017-09-14 10:46:12 -0300

    primeiro commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/xpto', function(req, res, next) {
+  res.json({status: "funcionando", versao: "0.0.1"})
+});
+
+module.exports = router;
